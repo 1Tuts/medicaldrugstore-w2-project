9a960f4cec6a5ebbbef9b6a558499bc0b6a45a22
golnaz hamishebahar <hamishebahar.1989@gmail.com>
2013-01-23 11:35:18 -0800

    inc

---
+++ mvc/template/inc/footer.php
@@ -0,0 +1,6 @@
+ </div>
+    <div class="backimg"></div>
+    <div class="backefct"></div>
+    <div class="footer"></div>
+</body>
+</html>
---
+++ mvc/template/inc/header.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<link href="./images/public.css" type="text/css" rel="stylesheet" />
+<meta charset="utf-8">
+<title></title>
+</head>
+
+<body>
+ 	<div class="header"> </div>
+	<div class="container">
+   		
